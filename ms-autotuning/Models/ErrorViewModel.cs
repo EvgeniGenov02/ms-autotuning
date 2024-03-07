f899883d74b89e2b59873de2b3985ff1e11cddff
Evgeni Genov <e.m.genov@abv.bg>
2024-03-07 10:07:50 +0200

    Add project files.

---
+++ ms-autotuning/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ms_autotuning.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
