c888e6ffccf99d94a130bff88345cf195572f76d
Evgeni Genov <e.m.genov@abv.bg>
2024-03-07 11:21:50 +0200

    Remove privacy

--- ms-autotuning/Controllers/HomeController.cs
+++ ms-autotuning/Controllers/HomeController.cs
@@ -18,11 +18,6 @@ namespace ms_autotuning.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
