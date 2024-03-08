7bcab7e01239493946c9c1059716bc9ec9553e30
Evgeni Genov <e.m.genov@abv.bg>
2024-03-08 20:27:15 +0200

    Add ContactForm and create index View

---
+++ ms-autotuning/Components/ContactForm.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ms_autotuning.Components
+{
+    public class ContactForm : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return await Task.FromResult<IViewComponentResult>(View("ContactForm"));
+        }
+    }
+}
